8111d5263babef97821f18c7ab841e62cfd0a689
Grzegorz <grzegorztarkowski8@wp.pl>
2021-08-04 09:44:46 +0200

    After Episode 3

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 import React from 'react';
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.header}>
+        <Text style={styles.boldText}>Hello World!</Text>
+      </View>
+      <View style={styles.body}>
+        <Text style={styles.boldText}>Lorem ipsum</Text>
+        <Text>Lorem ipsum</Text>
+        <Text>Lorem ipsum</Text>
+        <Text>Lorem ipsum</Text>
+      </View>
     </View>
   );
 }
@@ -14,8 +20,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#fff',
+  },
+  header: {
+    padding: 20,
+    backgroundColor: 'pink',
+  },
+  boldText: {
+    fontWeight: 'bold',
+  },
+  body: {
+    padding: 30,
+    backgroundColor: 'yellow',
   },
 });
